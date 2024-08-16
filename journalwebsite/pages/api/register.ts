dd0623a256fed652cc7b122d25a9e7714b4ae56a
WilyHyperion <ns9316@pleasantonusd.net>
2024-08-15 20:59:21 -0700

    Register now logs in and redirects to home, country select

--- journalwebsite/pages/api/register.ts
+++ journalwebsite/pages/api/register.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import  Prisma from '@/lib/prisma';
 import * as argon2 from "argon2";
+import { sign } from "jsonwebtoken";
 
 export  default async function handler(
   req: NextApiRequest,
@@ -19,13 +20,16 @@ export  default async function handler(
         let salt = require('crypto').randomBytes(32).toString('hex');
         let hashinput = req.body.password + salt;
         let hash = await argon2.hash(hashinput);
-        await Prisma.user.create({
+        let user = await Prisma.user.create({
         data: {
             name: req.body.username,
             passwordHash: hash,
+            country: req.body.country,
             salt: salt
         }
         });
+        const token = sign({ username: req.body.username }, process.env.JWT_SECRET as string, {  });
+        res.setHeader('Set-Cookie', [`token=${token}; HttpOnly; SameSite=Strict; Path=/`, `username=${user.name}; SameSite=Strict; Path=/`, `id=${user.id}; Path=/`]);
         res.status(200).json({ message: "User created" });
     }
 }
--- journalwebsite/pages/register.tsx
+++ journalwebsite/pages/register.tsx
@@ -5,9 +5,11 @@ import styles from "@/styles/Login.module.css";
 import Navigation from "./sidebar";
 import { useState } from "react";
 import Popup from "./popup";
-
+const countries = ["Select a Country ", "Afghanistan","Albania","Algeria","American Samoa","Andorra","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas (the)","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia (Plurinational State of)","Bonaire, Sint Eustatius and Saba","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory (the)","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cabo Verde","Cambodia","Cameroon","Canada","Cayman Islands (the)","Central African Republic (the)","Chad","Chile","China","Christmas Island","Cocos (Keeling) Islands (the)","Colombia","Comoros (the)","Congo (the Democratic Republic of the)","Congo (the)","Cook Islands (the)","Costa Rica","Croatia","Cuba","Curaçao","Cyprus","Czechia","Côte d'Ivoire","Denmark","Djibouti","Dominica","Dominican Republic (the)","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Eswatini","Ethiopia","Falkland Islands (the) [Malvinas]","Faroe Islands (the)","Fiji","Finland","France","French Guiana","French Polynesia","French Southern Territories (the)","Gabon","Gambia (the)","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea-Bissau","Guyana","Haiti","Heard Island and McDonald Islands","Holy See (the)","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran (Islamic Republic of)","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Korea (the Democratic People's Republic of)","Korea (the Republic of)","Kuwait","Kyrgyzstan","Lao People's Democratic Republic (the)","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macao","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands (the)","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia (Federated States of)","Moldova (the Republic of)","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands (the)","New Caledonia","New Zealand","Nicaragua","Niger (the)","Nigeria","Niue","Norfolk Island","Northern Mariana Islands (the)","Norway","Oman","Pakistan","Palau","Palestine, State of","Panama","Papua New Guinea","Paraguay","Peru","Philippines (the)","Pitcairn","Poland","Portugal","Puerto Rico","Qatar","Republic of North Macedonia","Romania","Russian Federation (the)","Rwanda","Réunion","Saint Barthélemy","Saint Helena, Ascension and Tristan da Cunha","Saint Kitts and Nevis","Saint Lucia","Saint Martin (French part)","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Sint Maarten (Dutch part)","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and the South Sandwich Islands","South Sudan","Spain","Sri Lanka","Sudan (the)","Suriname","Svalbard and Jan Mayen","Sweden","Switzerland","Syrian Arab Republic","Taiwan","Tajikistan","Tanzania, United Republic of","Thailand","Timor-Leste","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands (the)","Tuvalu","Uganda","Ukraine","United Arab Emirates (the)","United Kingdom of Great Britain and Northern Ireland (the)","United States Minor Outlying Islands (the)","United States of America (the)","Uruguay","Uzbekistan","Vanuatu","Venezuela (Bolivarian Republic of)","Viet Nam","Virgin Islands (British)","Virgin Islands (U.S.)","Wallis and Futuna","Western Sahara","Yemen","Zambia","Zimbabwe","Åland Islands"
+];
 export default function Login() {
   const [popup, setPopup] = useState(null as string | null);
+
   return (
     <div className={styles.container}>
       <Navigation/>
@@ -16,11 +18,28 @@ export default function Login() {
       <input type="text" placeholder="Username" id="username"/>
       <input type="password" placeholder="Password" id="password"/>
       <input type="password" placeholder="Repeat Password" id="repeat"/>
+     <select  id = "country" name = "country" style = {{
+        backgroundColor: "transparent",
+        color: "white",
+        border: "2px solid #115ed4",
+        padding: "0.5em",
+        textAlign: "center",
+        borderRadius: "0.5em",
+        fontSize: "1.5em",
+        width: "45%",
+      }}>
+        {countries.map((country) => {
+          return <option value = {country} style = {{
+            color: "black",
+          }}>{country}</option>
+        })}
+      </select>
       <div className={styles.buttonsContainer}>
         <button className="button-primary" onClick={async  () => {
             const username = (document.getElementById("username") as HTMLInputElement).value;
             const password = (document.getElementById("password") as HTMLInputElement).value;
             const repeat = (document.getElementById("repeat") as HTMLInputElement).value;
+            const country = (document.getElementById("country") as HTMLSelectElement).value;
             if(password != repeat){
                 console.log("Passwords do not match");
                 setPopup("Passwords do not match");
@@ -34,16 +53,20 @@ export default function Login() {
               setPopup("Password must be at least 8 characters long");
               return;
             }
+            if(country == "Select a Country "){
+              setPopup("Please select a country");
+              return;
+            }
             const response = await fetch("/api/register", {
               method: "POST",
-              body: JSON.stringify({ username, password }),
+              body: JSON.stringify({ username, password, country }),
               headers: {
                 "Content-Type": "application/json",
               },
             });
             if (response.ok) {
 
-              window.location.href = "/login";
+              window.location.href = "/logged/home";
             } else {
               setPopup("Username already exists");
             }
