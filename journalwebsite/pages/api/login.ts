205a7d064317bef9b4bd86fdc26d940b85ed334a
WilyHyperion <ns9316@pleasantonusd.net>
2024-08-31 21:22:08 -0700

    Change hasing algo to work on edge functions

--- journalwebsite/pages/api/login.ts
+++ journalwebsite/pages/api/login.ts
@@ -1,8 +1,9 @@
 
 import type { NextApiRequest, NextApiResponse } from "next";
 import  Prisma from '@/lib/prisma';
-import * as argon2 from "argon2";
+
 import { sign } from 'jsonwebtoken';
+import { compareSync } from "bcrypt-ts";
 export  default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -13,8 +14,7 @@ export  default async function handler(
     }
   });
   if(user){
-    let hashinput = req.body.password + user.salt;
-    if (user.passwordHash && await argon2.verify(user.passwordHash, hashinput)) {
+    if (user.passwordHash &&  compareSync( req.body.password + user.salt, user.passwordHash)) {
       const token = sign({ username: user.name }, process.env.JWT_SECRET as string, {  });
       res.setHeader('Set-Cookie', [`token=${token}; Secure; HttpOnly; SameSite=Strict; Path=/`, `username=${user.name}; SameSite=Strict; Path=/`, `id=${user.id}; Path=/`]); //IOS browsers will not send secure cookies across http even if its only sent locally for some reason. For the sake of testing on IOS ive removed it. 
       res.status(200).json({ message: "Logged in" });
